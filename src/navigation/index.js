8437018ec7dd50d4499017f506731e24db6a64d1
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-03-16 17:42:45 +0300

    .

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -163,7 +163,7 @@ const start = function (data, ID) {
 
               <section class="investing">
                 <center>
-                  <h2 class="general-title">Investing</h2>
+                  <h2 class="general-title">Advantages</h2>
                   <span>
                     The benefits of investing now on the Pre Seed round:
                   </span>
@@ -309,7 +309,7 @@ const start = function (data, ID) {
                 </div>
               </section>
               <section class="parthers" id="partners">
-                <h2 class="general-title">Partners & Bakers</h2>
+                <h2 class="general-title">Partners + Bakers</h2>
                 <div class="parthers-inner">
                   <a target="_blank" href="https://zyx.network/">
                     <img class="parther-img" src={svg["partners/partner1"]} />
@@ -347,74 +347,94 @@ const start = function (data, ID) {
                   <div class="roadmap-item">
                     <h6 class="roadmap-title">Q3 2022</h6>
                     <div class="roadmap-desc">
-                      <p>Idea Generation</p>
-                      <p>Market Analysis</p>
-                      <p>WhitePaper v1.0</p>
-                      <p>Pre-Seed round launch</p>
-                      <p>Launch of the website and socials</p>
-                      <p>Team building and expansion</p>
-                      <p>Finalization of the concept and wrapping</p>
-                      <p>Launch of the platform development</p>
+                      <p>Personal Account Development</p>
+                      <p>Registration of the Company</p>
+                      <p>Start Information Recourse Development</p>
+                      <p>Search for Investors & Affiliates</p>
+                      <p>Strategic partnerships</p>
+                      <p>Launching Landing page</p>
+                      <p>Launching Subpage on Site</p>
                     </div>
                   </div>
                   <div class="roadmap-item">
                     <h6 class="roadmap-title">Q3 2022</h6>
                     <div class="roadmap-desc">
-                      <p>Idea Generation</p>
-                      <p>Market Analysis</p>
-                      <p>WhitePaper v1.0</p>
-                      <p>Pre-Seed round launch</p>
-                      <p>Launch of the website and socials</p>
-                      <p>Team building and expansion</p>
-                      <p>Finalization of the concept and wrapping</p>
-                      <p>Launch of the platform development</p>
+                      <p>Negotiations with VC & DAO</p>
+                      <p>Integration of Neural Networks in work</p>
+                      <p>Launching Social Network & Automatization</p>
+                      <p>Preparation of sites for attracting traffic</p>
+                      <p>MVP, Testing</p>
+                      <p>Launching Personal Account in System</p>
+                      <p>First Projects on Listing</p>
+                      <p>First Airdrop</p>
+                      <p>Alpha Version of The Information Recourse</p>
+                      <p>Launch of Referral System</p>
+                      <p>Closing of the PreSeed Round</p>
+                      <p>Open of the Seed Round</p>
+                      <p>The First Investment from VC</p>
+                      <p>Partnership with KOLs and PRs</p>
+                      <p>Lead Investor Partnership</p>
                     </div>
                   </div>
                   <div class="roadmap-item">
                     <h6 class="roadmap-title">Q3 2022</h6>
                     <div class="roadmap-desc">
-                      <p>Idea Generation</p>
-                      <p>Market Analysis</p>
-                      <p>WhitePaper v1.0</p>
-                      <p>Pre-Seed round launch</p>
-                      <p>Launch of the website and socials</p>
-                      <p>Team building and expansion</p>
-                      <p>Finalization of the concept and wrapping</p>
-                      <p>Launch of the platform development</p>
+                      <p>Closing a Seed Round</p>
+                      <p>Open of Strategic Round</p>
+                      <p>Marketing Events</p>
+                      <p>The Alpha Version of the Ecosystem</p>
+                      <p>First Fundraisings on the Platform</p>
+                      <p>Launching Academy Development</p>
+                      <p>Implementation of Info-Bot</p>
+                      <p>Negotiations with CEX and DEX platforms</p>
+                      <p>Closing a Strategic Round</p>
+                      <p>Launch of Private Round</p>
+                      <p>NFT collection Developing</p>
+                      <p>Marketplace and INO development</p>
+                      <p>Customer and User Scaling</p>
+                      <p>Mobile application development</p>
+                      <p>Closing a Private Round</p>
                     </div>
                   </div>
                   <div class="roadmap-item">
                     <h6 class="roadmap-title">Q3 2022</h6>
                     <div class="roadmap-desc">
-                      <p>Idea Generation</p>
-                      <p>Market Analysis</p>
-                      <p>WhitePaper v1.0</p>
-                      <p>Pre-Seed round launch</p>
-                      <p>Launch of the website and socials</p>
-                      <p>Team building and expansion</p>
-                      <p>Finalization of the concept and wrapping</p>
-                      <p>Launch of the platform development</p>
+                      <p>Launch of Public Round on IDO</p>
+                      <p>Launch of NFT Sale</p>
+                      <p>Launch DAO, Staking, LP</p>
+                      <p>Closing a Public Round</p>
+                      <p>Development of bot-assistant</p>
+                      <p>First Educational Programs</p>
+                      <p>Firtst Alpha Mobile Application</p>
+                      <p>TGE and Listing Listing on CEX and DEX</p>
+                      <p>Development of P2P Exchanger</p>
+                      <p>Launch and Testing of the Marketplace</p>
+                      <p>Educational software integration</p>
+                      <p>Launch and testing of the Online Academy</p>
+                      <p>Scaling all directions of the ecosystem</p>
+                      <p>Holding marketing events</p>
+                      <p>Launch a Markentlace on Platform</p>
                     </div>
                   </div>
                   <div class="roadmap-item">
                     <h6 class="roadmap-title">Q3 2022</h6>
                     <div class="roadmap-desc">
-                      <p>Idea Generation</p>
-                      <p>Market Analysis</p>
-                      <p>WhitePaper v1.0</p>
-                      <p>Pre-Seed round launch</p>
-                      <p>Launch of the website and socials</p>
-                      <p>Team building and expansion</p>
-                      <p>Finalization of the concept and wrapping</p>
-                      <p>Launch of the platform development</p>
+                      <p>Deployment of the trading platform</p>
+                      <p>Wallet development</p>
+                      <p>Promotion of the Online Academy</p>
+                      <p>Opening affiliate branches</p>
+                      <p>Emphasis on patner formation</p>
+                      <p>Active ecosystem and brand PR</p>
+                      <p>Expanding the community of the entire ecosystem</p>
+                      <p>Analysis and fine-tuning of the entire system</p>
+                      <p>Official opening of all ecosystem products</p>
                     </div>
                   </div>
                 </div>
               </section>
               <section class="team" id="team">
-                <a name="team"></a>
                 <center>
-                  <h2 class="general-title">Multinational team</h2>
+                  <h2 class="general-title">Core team</h2>
                   <span>from different countries</span>
                 </center>
                 <div class="team-inner">
--- src/navigation/personal/wallet.js
+++ src/navigation/personal/wallet.js
@@ -39,33 +39,119 @@ const start = function (data, ID) {
                 <section class="main mb-25  ">
                   <h2 class="general-title mt-25">Wallet</h2>
                   <div class="main-blocks mt-20">
-                    <div class="blocks-item interesting">
-                      <span class="text-category text">
-                        Вам будет интересно
-                      </span>
-                      <span class="soon-text">coming soon</span>
-                    </div>
                     <div class="blocks-item wallet">
-                      <div class="user-icon">
-                        <img src={svg["iconsGreen/bag"]}></img>
-                      </div>
                       <div class="nums">
                         <span class="num_big">17,805</span>
                         <span class="num_small">+1,500$</span>
                         <span class="num_small"> +4,17%</span>
                       </div>
-                      <button class="btn btn-white">ИНВЕСТИРОВАТЬ</button>
-                      <button class="btn btn-white">КУПИТЬ</button>
-                      <button class="btn btn-white">ПРОДАТЬ</button>
-                      <button class="btn btn-white">ВВОД</button>
-                      <button class="btn btn-white">ВЫВОД</button>
-                      <button class="btn btn-white">ОБМЕН</button>
+                      <div class="wallet-btns">
+                        <button class="btn btn-white">ИНВЕСТИРОВАТЬ</button>
+                        <button class="btn btn-white">КУПИТЬ</button>
+                        <button class="btn btn-white">ПРОДАТЬ</button>
+                        <button class="btn btn-white">ВВОД</button>
+                        <button class="btn btn-white">ВЫВОД</button>
+                        <button class="btn btn-white">ОБМЕН</button>
+                      </div>
                     </div>
-                    <div class="blocks-item"></div>
+                    <div class="blocks-item reward">
+                      <div class="reward-col">
+                        <span>Reward</span>
+                        <span>рефки</span>
+                        <span>токены которые приходят дистрибьюцией</span>
+                        <span>пассивный доход</span>
+                        <span>начисления стейкинга</span>
+                        <span>ЛП начисления</span>
+                      </div>
+                      <div class="reward-col">
+                        <span
+                          class="text-green"
+                          style="justify-content:flex-end;display:flex;"
+                        >
+                          15,530$ (+4,17%)
+                        </span>
+                        <span
+                          class="text-green"
+                          style="justify-content:flex-end;display:flex;"
+                        >
+                          +1,500$
+                        </span>
+                        <span
+                          class="text-green"
+                          style="justify-content:flex-end;display:flex;"
+                        >
+                          +1,500$
+                        </span>
+                        <span
+                          class="text-green"
+                          style="justify-content:flex-end;display:flex;"
+                        >
+                          +1,500$
+                        </span>
+                        <span
+                          class="text-green"
+                          style="justify-content:flex-end;display:flex;"
+                        >
+                          +1,500$
+                        </span>
+                        <span
+                          class="text-green"
+                          style="justify-content:flex-end;display:flex;"
+                        >
+                          +1,500$
+                        </span>
+                      </div>
+                    </div>
+                    <div class="blocks-item lock">
+                      <div class="lock-col">
+                        <span>Reward</span>
+                        <span>рефки</span>
+                        <span>токены которые приходят дистрибьюцией</span>
+                        <span>пассивный доход</span>
+                        <span>начисления стейкинга</span>
+                        <span>ЛП начисления</span>
+                      </div>
+                      <div class="lock-col">
+                        <span
+                          class="text-red"
+                          style="justify-content:flex-end;display:flex;"
+                        >
+                          15,530$
+                        </span>
+                        <span
+                          class="text-red"
+                          style="justify-content:flex-end;display:flex;"
+                        >
+                          +1,500$
+                        </span>
+                        <span
+                          class="text-red"
+                          style="justify-content:flex-end;display:flex;"
+                        >
+                          +1,500$
+                        </span>
+                        <span
+                          class="text-red"
+                          style="justify-content:flex-end;display:flex;"
+                        >
+                          +1,500$
+                        </span>
+                        <span
+                          class="text-red"
+                          style="justify-content:flex-end;display:flex;"
+                        >
+                          +1,500$
+                        </span>
+                        <span
+                          class="text-red"
+                          style="justify-content:flex-end;display:flex;"
+                        >
+                          +1,500$
+                        </span>
+                      </div>
+                    </div>
                     <div class="blocks-item news">
-                      <span class="text-category text">
-                        Вам будет интересно
-                      </span>
+                      <span class="text-category text">NFT</span>
                       <span class="soon-text">coming soon</span>
                     </div>
                   </div>
