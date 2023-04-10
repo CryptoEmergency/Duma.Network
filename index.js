1569a5c286566947fc61d82950a7b0cdf2740162
Betarost <betarost@yandex.ru>
2023-04-10 17:58:22 +0300

    .

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-import { ServerInit, ServerBuild, ServerStart } from "@betarost/cemserver";
+import { CEM } from "@betarost/cemserver";
 import path from "path";
 import dotenv from "dotenv";
 dotenv.config();
 
-const portApi = 9092;
-const portExpress = 53535;
+
+const port = 80;
 const target = "duma.network";
 
-const optionInit = {
-  port: 80,
-  target,
+
+const options = {
+  port,
   hotReload: true,
-  watch: true,
-  mode: "development",
+  allowedHosts: [target],
   path: {
     src: path.resolve("app.js"),
     public: path.resolve("public"),
     fileName: "main.[fullhash].js",
     template: path.resolve("src/template/index.html"),
   },
-  allowedHosts: [target],
   proxy: {
     "/api/v2": {
       target: `https://${target}`,
@@ -44,11 +42,4 @@ if (process.env.DISABLERELOAD) {
   optionInit.hotReload = false
 }
 
-const start = async function () {
-  ServerInit(optionInit);
-  ServerBuild({}).then((result) => {
-    if (result) ServerStart(result);
-  });
-};
-
-start();
+CEM.start(options)
